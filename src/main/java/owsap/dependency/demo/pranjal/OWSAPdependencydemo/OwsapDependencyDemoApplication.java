29c947b7ccf6dda3ea82bca0eb3a3f3e7057fe15
Pranjal Medhi <50314615+pranzm@users.noreply.github.com>
2023-09-12 07:45:41 +0530

    Initial commit

---
+++ src/main/java/owsap/dependency/demo/pranjal/OWSAPdependencydemo/OwsapDependencyDemoApplication.java
@@ -0,0 +1,13 @@
+package owsap.dependency.demo.pranjal.OWSAPdependencydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwsapDependencyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwsapDependencyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/owsap/dependency/demo/pranjal/OWSAPdependencydemo/OwsapDependencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package owsap.dependency.demo.pranjal.OWSAPdependencydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwsapDependencyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
